ac4e2574cf2df65466a0b46b419993b9bf24191a
zafranf <zafranfadilah@gmail.com>
2019-12-19 20:54:56 +0700

    remove contextmenu on mobile

--- src/resources/views/AdminSC/content/pages_form.blade.php
+++ src/resources/views/AdminSC/content/pages_form.blade.php
@@ -1,66 +1,70 @@
 @extends('zetthcore::AdminSC.layouts.main')
 
 @section('content')
-	<div class="panel-body">
-		<form class="form-horizontal" action="{{ url($current_url) }}{{ isset($data) ? '/' . $data->id : '' }}" method="post">
-			<div class="form-group">
-				<label for="title" class="col-sm-2 control-label">Judul</label>
-				<div class="col-sm-10">
-					<input type="text" class="form-control autofocus" id="title" name="title" value="{{ isset($data) ? $data->title : old('title') }}" maxlength="100" placeholder="Judul halaman..">
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="slug" class="col-sm-2 control-label">Tautan</label>
-				<div class="col-sm-10">
-					<div class="input-group">
-						<span class="input-group-addon" id="slug_span">{{ url("/") }}/</span>
-						<input type="text" id="slug" class="form-control" name="slug" placeholder="Sesuaikan tautan.." value="{{ isset($data) ? $data->slug : old('slug') }}" {{ isset($data) ? 'readonly' : '' }}>
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<label for="content" class="col-sm-2 control-label">Konten</label>
-				<div class="col-sm-10">
-					<textarea id="content" name="content" class="form-control" placeholder="Tulis konten di sini..">{{ isset($data) ? $data->content : old('content') }}</textarea>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
-					<div class="checkbox">
-						<label>
-							<input type="checkbox" name="status" {{ (isset($data) && $data->status == 0) ? '' : 'checked' }}> Aktif
-						</label>
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-offset-2 col-sm-10">
-          {{ isset($data) ? method_field('PUT') : '' }}
-          {{ csrf_field() }}
-				  {{ _get_button_post($current_url, true, $data->id ?? '') }}
-				</div>
-			</div>
-		</form>
-	</div>
+<div class="panel-body">
+  <form class="form-horizontal" action="{{ url($current_url) }}{{ isset($data) ? '/' . $data->id : '' }}" method="post">
+    <div class="form-group">
+      <label for="title" class="col-sm-2 control-label">Judul</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control autofocus" id="title" name="title"
+          value="{{ isset($data) ? $data->title : old('title') }}" maxlength="100" placeholder="Judul halaman..">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="slug" class="col-sm-2 control-label">Tautan</label>
+      <div class="col-sm-10">
+        <div class="input-group">
+          <span class="input-group-addon" id="slug_span">{{ url("/") }}/</span>
+          <input type="text" id="slug" class="form-control" name="slug" placeholder="Sesuaikan tautan.."
+            value="{{ isset($data) ? $data->slug : old('slug') }}" {{ isset($data) ? 'readonly' : '' }}>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="content" class="col-sm-2 control-label">Konten</label>
+      <div class="col-sm-10">
+        <textarea id="content" name="content" class="form-control"
+          placeholder="Tulis konten di sini..">{{ isset($data) ? $data->content : old('content') }}</textarea>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" name="status" {{ (isset($data) && $data->status == 0) ? '' : 'checked' }}> Aktif
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        {{ isset($data) ? method_field('PUT') : '' }}
+        {{ csrf_field() }}
+        {{ _get_button_post($current_url, true, $data->id ?? '') }}
+      </div>
+    </div>
+  </form>
+</div>
 @endsection
 
 @section('styles')
-  <style>
-    .mce-fullscreen {
-        z-index: 9999!important;
-    }
-    #mceu_9 {
-      position: absolute;
-      right: 4px;
-      top: 4px;
-    }
-  </style>
+<style>
+  .mce-fullscreen {
+    z-index: 9999 !important;
+  }
+
+  #mceu_9 {
+    position: absolute;
+    right: 4px;
+    top: 4px;
+  }
+</style>
 @endsection
 
 @section('scripts')
-  {!! _admin_js('themes/admin/AdminSC/plugins/tinymce/4.3.2/tinymce.min.js') !!}
-  <script>
-    $(document).ready(function(){
+{!! _admin_js('themes/admin/AdminSC/plugins/tinymce/4.3.2/tinymce.min.js') !!}
+<script>
+  $(document).ready(function(){
       @if (!isset($data))
         $('#title').on('keyup blur', function(){
           var slug = _get_slug($(this).val());
@@ -82,7 +86,7 @@
         plugins: [
           "advlist autolink link image lists charmap print preview hr anchor pagebreak",
           "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
-          "table contextmenu directionality emoticons paste textcolor responsivefilemanager code",
+          "table {{ app('is_desktop') ? 'contextmenu ' : '' }}directionality emoticons paste textcolor responsivefilemanager code",
           "placeholder youtube fullscreen"
         ],
         toolbar1: "undo redo | bold italic underline blockquote | link unlink | image | fullscreen",
@@ -104,5 +108,5 @@
         }
       });
     });
-  </script>
+</script>
 @endsection
