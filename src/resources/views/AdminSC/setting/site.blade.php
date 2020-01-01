01efc09578a8e22febba90c3fbb07fcb92bbc8ed
zafranf <zafranfadilah@gmail.com>
2020-01-02 03:19:14 +0700

    fix layout

--- src/resources/views/AdminSC/setting/site.blade.php
+++ src/resources/views/AdminSC/setting/site.blade.php
@@ -106,210 +106,195 @@
               placeholder="Nomor telepon.." maxlength="16">
           </div>
         </div>
-        {{-- <div class="form-group" {!! (Auth::user()->id != 1)?'style="display:none;"':'' !!}>
-						<label for="max_login_failed" class="col-md-4 control-label">Max Login Failed</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control" name="max_login_failed" value="{{ app('site')->max_login_failed ?? '' }}"
-        maxlength="1">
-        <span class="text-danger">{{ ($errors->has('max_login_failed'))?$errors->first('max_login_failed'):'' }}</span>
-      </div>
-    </div> --}}
-    {{-- <div class="form-group" {!! (Auth::user()->id != 1)?'style="display:none;"':'' !!}>
-						<label for="lockout_time" class="col-md-4 control-label">Lockout Time <small>(in minutes)</small></label>
-						<div class="col-md-8">
-							<input type="text" class="form-control" name="lockout_time" value="{{ app('site')->lockout_time ?? '' }}"
-    maxlength="2">
-    <span class="text-danger">{{ ($errors->has('lockout_time'))?$errors->first('lockout_time'):'' }}</span>
-</div>
-</div> --}}
-<div class="form-group" {!! (Auth::user()->id != 1)?'style="display:none;"':'' !!}>
-  <label for="perpage" class="col-md-4 control-label">Data Perhalaman</label>
-  <div class="col-md-8">
-    <input type="number" class="form-control" id="perpage" name="perpage" value="{{ app('site')->perpage ?? 0 }}"
-      placeholder="Jumlah data perhalaman.." min="3" max="100">
-  </div>
-</div>
-<div class="form-group">
-  <label for="enable" class="col-md-4 control-label">
-    Akses Pengunjung
-    {{-- <small class="help-block">Fitur untuk pengunjung situs</small> --}}
-  </label>
-  <div class="col-md-8">
-    <div class="checkbox">
-      <div class="col-xs-6 col-sm-3">
-        <label>
-          <input type="checkbox" id="enable-subscribe" name="enable_subscribe"
-            {{ (!app('site')->enable_subscribe) ? '' : 'checked' }}> Langganan
-        </label>
-      </div>
-      <div class="col-xs-6 col-sm-3">
-        <label>
-          <input type="checkbox" id="enable-comment" name="enable_comment"
-            {{ (!app('site')->enable_comment) ? '' : 'checked' }}> Komentar
-        </label>
-      </div>
-      <div class="col-xs-6 col-sm-3">
-        <label>
-          <input type="checkbox" id="enable-like" name="enable_like" {{ (!app('site')->enable_like) ? '' : 'checked' }}>
-          Suka
-        </label>
-      </div>
-      <div class="col-xs-6 col-sm-3">
-        <label>
-          <input type="checkbox" id="enable-share" name="enable_share"
-            {{ (!app('site')->enable_share) ? '' : 'checked' }}> Sebar
-        </label>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="form-group">
-  <label for="status" class="col-md-4 control-label">Status</label>
-  <div class="col-md-8">
-    <select class="form-control custom-select2" id="status" name="status">
-      <option value="1" {{ (app('site')->status == 1) ? 'selected' : '' }}>Aktif</option>
-      <option value="0" {{ (app('site')->status == 0) ? 'selected' : '' }}>Segera Hadir</option>
-      <option value="2" {{ (app('site')->status == 2) ? 'selected' : '' }}>Perbaikan</option>
-    </select>
-  </div>
-</div>
-<div class="form-group" {!! (app('site')->status == 1) ? 'style="display:none;"' : '' !!} id="d-active-at">
-  <label for="active_at" class="col-md-4 control-label">Aktif pada</label>
-  <div class="col-md-8">
-    <input type="text" class="form-control" id="active-at" name="active_at"
-      value="{{ isset(app('site')->active_at) ? carbon(app('site')->active_at)->format('Y-m-d') : '' }}" {!!
-      (app('site')->status == 1) ? 'readonly' : '' !!} placeholder="Dibuka pada..">
-  </div>
-</div>
-</div>
-<div class="col-md-6">
-  <h4>Media Sosial <span class="btn btn-default btn-xs pull-right" id="btn-add-socmed"><i class="fa fa-plus"></i></span>
-  </h4>
-  <hr>
-  <div class="form-group">
-    <label for="label" class="col-md-4 control-label">
-      Akun
-      <small class="help-block">Akun sosial media situs</small>
-    </label>
-    <div class="col-md-8">
-      @if (isset($socmed_data) && count($socmed_data) > 0)
-      @foreach ($socmed_data as $key => $val)
-      @php
-      $rand = rand(111111111, 999999999);
-      @endphp
-      <div id="div-socmed-{{ $rand }}">
-        <div class="col-md-3 col-xs-6 no-padding">
-          <select class="form-control custom-select2" id="socmed-id-{{ $rand }}" name="socmed_id[]">
-            <option value="">--Pilih--</option>
-            @if (isset($socmeds))
-            @foreach ($socmeds as $socmed)
-            @php
-            $sl = ($socmed->id==$val->socmed->id) ? 'selected' : ''
-            @endphp
-            <option value="{{ $socmed->id }}" {{ $sl }}>{{ $socmed->name }}</option>
-            @endforeach
-            @endif
-          </select>
-        </div>
-        <div class="col-md-9 col-xs-6 no-padding">
-          @if ($key > 0)
-          <div class="input-group">
-            <input type="text" class="form-control" id="socmed-uname-{{ $rand }}" name="socmed_uname[]"
-              placeholder="Nama/ID akun.." maxlength="50" value="{{ $val->username }}">
-            <span class="input-group-btn">
-              <button type="button" class="btn" style="background:white;border:1px solid #ccc;"
-                onclick="_remove('#div-socmed-{{ $rand }}')"><i class="fa fa-minus"></i></button>
-            </span>
+        <div class="form-group" {!! (Auth::user()->id != 1)?'style="display:none;"':'' !!}>
+          <label for="perpage" class="col-md-4 control-label">Data Perhalaman</label>
+          <div class="col-md-8">
+            <input type="number" class="form-control" id="perpage" name="perpage"
+              value="{{ app('site')->perpage ?? 0 }}" placeholder="Jumlah data perhalaman.." min="3" max="100">
           </div>
-          @else
-          <input type="text" class="form-control" id="socmed-uname-{{ $rand }}" name="socmed_uname[]"
-            placeholder="Nama/ID akun.." value="{{ $val->username }}">
-          @endif
+        </div>
+        <div class="form-group">
+          <label for="enable" class="col-md-4 control-label">
+            Akses Pengunjung
+            {{-- <small class="help-block">Fitur untuk pengunjung situs</small> --}}
+          </label>
+          <div class="col-md-8">
+            <div class="checkbox">
+              <div class="col-xs-6 col-sm-3">
+                <label>
+                  <input type="checkbox" id="enable-subscribe" name="enable_subscribe"
+                    {{ (!app('site')->enable_subscribe) ? '' : 'checked' }}> Langganan
+                </label>
+              </div>
+              <div class="col-xs-6 col-sm-3">
+                <label>
+                  <input type="checkbox" id="enable-comment" name="enable_comment"
+                    {{ (!app('site')->enable_comment) ? '' : 'checked' }}> Komentar
+                </label>
+              </div>
+              <div class="col-xs-6 col-sm-3">
+                <label>
+                  <input type="checkbox" id="enable-like" name="enable_like"
+                    {{ (!app('site')->enable_like) ? '' : 'checked' }}>
+                  Suka
+                </label>
+              </div>
+              <div class="col-xs-6 col-sm-3">
+                <label>
+                  <input type="checkbox" id="enable-share" name="enable_share"
+                    {{ (!app('site')->enable_share) ? '' : 'checked' }}> Sebar
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="status" class="col-md-4 control-label">Status</label>
+          <div class="col-md-8">
+            <select class="form-control custom-select2" id="status" name="status">
+              <option value="1" {{ (app('site')->status == 1) ? 'selected' : '' }}>Aktif</option>
+              <option value="0" {{ (app('site')->status == 0) ? 'selected' : '' }}>Segera Hadir</option>
+              <option value="2" {{ (app('site')->status == 2) ? 'selected' : '' }}>Perbaikan</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group" {!! (app('site')->status == 1) ? 'style="display:none;"' : '' !!} id="d-active-at">
+          <label for="active_at" class="col-md-4 control-label">Aktif pada</label>
+          <div class="col-md-8">
+            <input type="text" class="form-control" id="active-at" name="active_at"
+              value="{{ isset(app('site')->active_at) ? carbon(app('site')->active_at)->format('Y-m-d') : '' }}" {!!
+              (app('site')->status == 1) ? 'readonly' : '' !!} placeholder="Dibuka pada..">
+          </div>
         </div>
       </div>
-      @endforeach
-      @else
-      <div class="col-md-3 col-xs-6 no-padding">
-        <select class="form-control custom-select2" id="socmed-id" name="socmed_id[]">
-          <option value="">--Pilih--</option>
-          @if (isset($socmeds))
-          @foreach ($socmeds as $socmed)
-          <option value="{{ $socmed->id }}">{{ $socmed->name }}</option>
-          @endforeach
-          @endif
-        </select>
+      <div class="col-md-6">
+        <h4>Media Sosial <span class="btn btn-default btn-xs pull-right" id="btn-add-socmed"><i
+              class="fa fa-plus"></i></span>
+        </h4>
+        <hr>
+        <div class="form-group">
+          <label for="label" class="col-md-4 control-label">
+            Akun
+            <small class="help-block">Akun sosial media situs</small>
+          </label>
+          <div class="col-md-8">
+            @if (isset($socmed_data) && count($socmed_data) > 0)
+            @foreach ($socmed_data as $key => $val)
+            @php
+            $rand = rand(111111111, 999999999);
+            @endphp
+            <div id="div-socmed-{{ $rand }}">
+              <div class="col-md-3 col-xs-6 no-padding">
+                <select class="form-control custom-select2" id="socmed-id-{{ $rand }}" name="socmed_id[]">
+                  <option value="">--Pilih--</option>
+                  @if (isset($socmeds))
+                  @foreach ($socmeds as $socmed)
+                  @php
+                  $sl = ($socmed->id==$val->socmed->id) ? 'selected' : ''
+                  @endphp
+                  <option value="{{ $socmed->id }}" {{ $sl }}>{{ $socmed->name }}</option>
+                  @endforeach
+                  @endif
+                </select>
+              </div>
+              <div class="col-md-9 col-xs-6 no-padding">
+                @if ($key > 0)
+                <div class="input-group">
+                  <input type="text" class="form-control" id="socmed-uname-{{ $rand }}" name="socmed_uname[]"
+                    placeholder="Nama/ID akun.." maxlength="50" value="{{ $val->username }}">
+                  <span class="input-group-btn">
+                    <button type="button" class="btn" style="background:white;border:1px solid #ccc;"
+                      onclick="_remove('#div-socmed-{{ $rand }}')"><i class="fa fa-minus"></i></button>
+                  </span>
+                </div>
+                @else
+                <input type="text" class="form-control" id="socmed-uname-{{ $rand }}" name="socmed_uname[]"
+                  placeholder="Nama/ID akun.." value="{{ $val->username }}">
+                @endif
+              </div>
+            </div>
+            @endforeach
+            @else
+            <div class="col-md-3 col-xs-6 no-padding">
+              <select class="form-control custom-select2" id="socmed-id" name="socmed_id[]">
+                <option value="">--Pilih--</option>
+                @if (isset($socmeds))
+                @foreach ($socmeds as $socmed)
+                <option value="{{ $socmed->id }}">{{ $socmed->name }}</option>
+                @endforeach
+                @endif
+              </select>
+            </div>
+            <div class="col-md-9 col-xs-6 no-padding">
+              <input type="text" class="form-control" id="socmed-uname" name="socmed_uname[]"
+                placeholder="Nama/ID akun.." maxlength="50">
+            </div>
+            @endif
+            <div id="div-socmed"></div>
+          </div>
+        </div>
+        <h4>Pengaturan SEO</h4>
+        <hr>
+        <div class="form-group">
+          <label for="keywords" class="col-md-4 control-label">
+            Kata Kunci
+            <small class="help-block">Tekan enter untuk menambahkan</small>
+          </label>
+          <div class="col-md-8">
+            <input type="text" class="form-control" id="keywords" name="keywords"
+              value="{{ app('site')->keywords ?? '' }}" placeholder="Kata kunci situs.." maxlength="50">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="description" class="col-md-4 control-label">Deskripsi</label>
+          <div class="col-md-8">
+            <textarea class="form-control" id="description" name="description" rows="4"
+              placeholder="Penjelasan singkat mengenai situs..">{{ app('site')->description ?? '' }}</textarea>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="google_analytics" class="col-md-4 control-label">
+            Google Analytics
+            <small class="help-block">Kode untuk analitik website dari Google</small>
+          </label>
+          <div class="col-md-8">
+            <input type="text" class="form-control" id="google-analytics" name="google_analytics"
+              value="{{ app('site')->google_analytics ?? '' }}" placeholder="Kode Google Analytics.." maxlength="20">
+          </div>
+        </div>
+        <h4>Lokasi</h4>
+        <hr>
+        <div class="form-group">
+          <label for="address" class="col-md-4 control-label">Alamat</label>
+          <div class="col-md-8">
+            <textarea class="form-control" id="address" name="address" rows="4" placeholder="Alamat lengkap.."
+              maxlength="280">{{ app('site')->address ?? '' }}</textarea>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="coordinate" class="col-md-4 control-label">
+            Nama Lokasi/Titik Koordinat
+            <small class="help-block">
+              (contoh:
+              <b>Monas</b> atau
+              <b>-6.1762332,106.8206543</b>)
+            </small>
+          </label>
+          <div class="col-md-8">
+            <input type="text" class="form-control" id="coordinate" name="coordinate"
+              value="{{ app('site')->coordinate ?? '' }}" placeholder="Titik koordinat.." maxlength="30">
+          </div>
+        </div>
       </div>
-      <div class="col-md-9 col-xs-6 no-padding">
-        <input type="text" class="form-control" id="socmed-uname" name="socmed_uname[]" placeholder="Nama/ID akun.."
-          maxlength="50">
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-2 col-md-10">
+        {{ isset(app('site')->id) ? method_field('PUT') : '' }}
+        {{ csrf_field() }}
+        {{ _get_button_post(url(app('admin_path') . '/dashboard')) }}
       </div>
-      @endif
-      <div id="div-socmed"></div>
     </div>
-  </div>
-  <h4>Pengaturan SEO</h4>
-  <hr>
-  <div class="form-group">
-    <label for="keywords" class="col-md-4 control-label">
-      Kata Kunci
-      <small class="help-block">Tekan enter untuk menambahkan</small>
-    </label>
-    <div class="col-md-8">
-      <input type="text" class="form-control" id="keywords" name="keywords" value="{{ app('site')->keywords ?? '' }}"
-        placeholder="Kata kunci situs.." maxlength="50">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="description" class="col-md-4 control-label">Deskripsi</label>
-    <div class="col-md-8">
-      <textarea class="form-control" id="description" name="description" rows="4"
-        placeholder="Penjelasan singkat mengenai situs..">{{ app('site')->description ?? '' }}</textarea>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="google_analytics" class="col-md-4 control-label">
-      Google Analytics
-      <small class="help-block">Kode untuk analitik website dari Google</small>
-    </label>
-    <div class="col-md-8">
-      <input type="text" class="form-control" id="google-analytics" name="google_analytics"
-        value="{{ app('site')->google_analytics ?? '' }}" placeholder="Kode Google Analytics.." maxlength="20">
-    </div>
-  </div>
-  <h4>Lokasi</h4>
-  <hr>
-  <div class="form-group">
-    <label for="address" class="col-md-4 control-label">Alamat</label>
-    <div class="col-md-8">
-      <textarea class="form-control" id="address" name="address" rows="4" placeholder="Alamat lengkap.."
-        maxlength="280">{{ app('site')->address ?? '' }}</textarea>
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="coordinate" class="col-md-4 control-label">
-      Titik Lokasi/Koordinat
-      <small class="help-block">
-        Garis lintang dan garis bujur.<br>
-        (contoh:
-        <b>Monas</b> atau
-        <b>-6.1762332,106.8206543</b>)
-      </small>
-    </label>
-    <div class="col-md-8">
-      <input type="text" class="form-control" id="coordinate" name="coordinate"
-        value="{{ app('site')->coordinate ?? '' }}" placeholder="Titik koordinat.." maxlength="30">
-    </div>
-  </div>
-</div>
-</div>
-<div class="form-group">
-  <div class="col-md-offset-2 col-md-10">
-    {{ isset(app('site')->id) ? method_field('PUT') : '' }}
-    {{ csrf_field() }}
-    {{ _get_button_post(url(app('admin_path') . '/dashboard')) }}
-  </div>
-</div>
-</form>
+  </form>
 </div>
 @endsection
 
