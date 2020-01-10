33825745c8034a017facad04f6fed39c3a889d8f
zafranf <zafranfadilah@gmail.com>
2020-01-10 18:13:52 +0700

    rename captcha key

--- src/app/helpers.php
+++ src/app/helpers.php
@@ -619,7 +619,7 @@ if (!function_exists('getCacheTime')) {
 if (!function_exists('validateCaptcha')) {
     function validateCaptcha($response)
     {
-        $recaptcha = new \ReCaptcha\ReCaptcha(env('GOOGLE_RECAPTCHA_KEY'));
+        $recaptcha = new \ReCaptcha\ReCaptcha(env('GOOGLE_RECAPTCHA_SECRET_KEY'));
         $res = $recaptcha->verify($response, _server('REMOTE_ADDR'));
 
         return $res->isSuccess();
