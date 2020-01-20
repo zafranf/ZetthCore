e5da3ae3db7616fdbe788ce7720bc3b628644720
zafranf <zafranfadilah@gmail.com>
2020-01-21 01:39:01 +0700

    change excerpt to string

--- src/database/migrations/2018_09_05_164321_create_posts_table.php
+++ src/database/migrations/2018_09_05_164321_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title')->index();
             $table->string('slug')->index();
             $table->longText('content');
-            $table->text('excerpt')->nullable();
+            $table->string('excerpt')->nullable();
             $table->string('cover')->nullable();
             $table->enum('type', ['article', 'page', 'video'])->default('article');
             $table->boolean('share')->unsigned()->default(0);
