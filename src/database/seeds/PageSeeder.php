91b5c2ecdfd1a91155bd414570fe0bdb33bc466f
zafranf <zafranfadilah@gmail.com>
2020-04-25 15:28:03 +0700

    fix page status seed

--- src/database/seeds/PageSeeder.php
+++ src/database/seeds/PageSeeder.php
@@ -18,7 +18,7 @@ class PageSeeder extends Seeder
             'slug' => 'about',
             'content' => '<p>Ini adalah contoh halaman <b>Tentang Kami</b>. Silakan sesuaikan isinya di sini.</p>',
             'type' => 'page',
-            'status' => 'yes',
+            'status' => 'active',
             'created_by' => 2,
         ]);
     }
