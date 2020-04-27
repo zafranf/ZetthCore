55d4aef13d0d6e3c306ff2f688b6ad9dd9f01328
zafranf <zafranfadilah@gmail.com>
2020-04-27 11:25:27 +0700

    add user lang attribute

--- src/app/Models/UserDetail.php
+++ src/app/Models/UserDetail.php
@@ -7,4 +7,10 @@ class UserDetail extends Base
     protected $primaryKey = 'user_id';
     protected $guarded = [];
     public $incrementing = false;
+    public $appends = ['lang'];
+
+    public function getLangAttribute()
+    {
+        return explode("_", $this->language ?? 'id_ID')[0];
+    }
 }
