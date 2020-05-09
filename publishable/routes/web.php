a73649db0cfba163b6e321b9c8fa26d2c236a9c0
zafranf <zafranfadilah@gmail.com>
2020-05-09 17:33:09 +0700

    remove default path

--- publishable/routes/web.php
+++ publishable/routes/web.php
@@ -55,43 +55,43 @@ Route::name('web.')->middleware(['site'])->group(function () {
     Route::middleware(['visitor_log'])->group(function () {
         /* check login status */
         Route::middleware(['guest'])->group(function () {
-            Route::get('/' . config('path.register', 'register'), 'Auth\LoginController@showRegistrationForm')->name('register');
-            Route::get('/' . config('path.verification', 'verification') . '/{type}', 'Auth\VerificationController@index')->name('verify');
-            Route::get('/' . config('path.login', 'login'), 'Auth\LoginController@showLoginForm')->name('login');
-            Route::get('/' . config('path.forgotpass', 'forgot-password'), 'Auth\ForgotPasswordController@index')->name('forgot.password');
-            Route::get('/' . config('path.resetpass', 'reset-password'), 'Auth\ResetPasswordController@index')->name('reset.password');
+            Route::get('/' . config('path.register'), 'Auth\LoginController@showRegistrationForm')->name('register');
+            Route::get('/' . config('path.verification') . '/{type}', 'Auth\VerificationController@index')->name('verify');
+            Route::get('/' . config('path.login'), 'Auth\LoginController@showLoginForm')->name('login');
+            Route::get('/' . config('path.forgotpass'), 'Auth\ForgotPasswordController@index')->name('forgot.password');
+            Route::get('/' . config('path.resetpass'), 'Auth\ResetPasswordController@index')->name('reset.password');
         });
 
         /* must logged in */
         Route::middleware(['auth'])->group(function () {
             /* GET */
-            Route::get('/' . config('path.profile', 'profile'), 'Site\UserController@index')->name('profile');
-            Route::get('/' . config('path.profile', 'profile') . '/edit', 'Site\UserController@edit')->name('profile.edit');
+            Route::get('/' . config('path.profile'), 'Site\UserController@index')->name('profile');
+            Route::get('/' . config('path.profile') . '/edit', 'Site\UserController@edit')->name('profile.edit');
         });
 
         /* Front Routes */
         Route::get('/', 'Site\MainController@index')->name('root');
-        Route::get('/' . config('path.search', 'search'), 'Site\MainController@search')->name('search');
+        Route::get('/' . config('path.search'), 'Site\MainController@search')->name('search');
 
         /* Gallery Routes */
-        Route::get('/' . config('path.albums', 'albums'), 'Site\GalleryController@album')->name('albums');
-        Route::get('/' . config('path.album', 'album') . '/{slug}', 'Site\GalleryController@album_detail')->name('album');
-        Route::get('/' . config('path.videos', 'videos'), 'Site\GalleryController@video')->name('videos');
-        Route::get('/' . config('path.video', 'video') . '/{slug}', 'Site\GalleryController@video_detail')->name('video');
+        Route::get('/' . config('path.albums'), 'Site\GalleryController@album')->name('albums');
+        Route::get('/' . config('path.album') . '/{slug}', 'Site\GalleryController@album_detail')->name('album');
+        Route::get('/' . config('path.videos'), 'Site\GalleryController@video')->name('videos');
+        Route::get('/' . config('path.video') . '/{slug}', 'Site\GalleryController@video_detail')->name('video');
 
         /* Misc Routes */
-        Route::get('/' . config('path.comingsoon', 'comingsoon'), 'Site\MiscController@comingsoon')->name('comingsoon');
-        Route::get('/' . config('path.maintenance', 'maintenance'), 'Site\MiscController@maintenance')->name('maintenance');
+        Route::get('/' . config('path.comingsoon'), 'Site\MiscController@comingsoon')->name('comingsoon');
+        Route::get('/' . config('path.maintenance'), 'Site\MiscController@maintenance')->name('maintenance');
 
         /* Posts by Routes */
-        Route::get('/{by_type}/{slug}', 'Site\PostController@by')->name('posts.by')->where('by_type', config('path.category', 'category') . '|' . config('path.tag', 'tag') . '|' . config('path.author', 'author'));
+        Route::get('/{by_type}/{slug}', 'Site\PostController@by')->name('posts.by')->where('by_type', config('path.category') . '|' . config('path.tag') . '|' . config('path.author'));
 
         /* Posts Routes */
-        Route::get('/{type}', 'Site\PostController@index')->name('posts')->where('type', config('path.posts', 'posts'));
-        Route::get('/{type}/{slug}', 'Site\PostController@detail')->name('post')->where('type', config('path.post', 'post'));
+        Route::get('/{type}', 'Site\PostController@index')->name('posts')->where('type', config('path.posts'));
+        Route::get('/{type}/{slug}', 'Site\PostController@detail')->name('post')->where('type', config('path.post'));
 
         /* Page Route */
-        Route::get('/' . config('path.contact', 'contact'), 'Site\PageController@contact')->name('contact');
+        Route::get('/' . config('path.contact'), 'Site\PageController@contact')->name('contact');
     });
 
     /* include additional menu site */
